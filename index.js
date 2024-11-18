88cf477f62dd64cfcb7b6a490c78179f7a14ce75
raffay123456789 <abdulraffay442@gmail.com>
2024-11-19 00:38:03 +0500

    New files added

---
+++ index.js
@@ -0,0 +1,70 @@
+// //This is my First Java Script code!
+// console.log('Hello World');  
+   
+// let name = "Abdur Raffay";
+// console.log('name');  
+
+//VARIABLES
+// let firstName = 'Abdur'; 
+// let lastName = 'Raffay'; //VARIABLES
+
+
+//CONSTANTS
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);  //CONSTANTS
+
+// console.log('Abdur Raffay'); 
+
+
+//PRIMITIVES/VALUETYPES
+// let name = 'Abdur Raffay'; //string literal
+// let age = 24; //number literal
+// let isApproved = true; // boolean literal 
+// let firstname = undefined;
+// let selectedColor = null ;//PRIMITIVES/VALUE TYPES
+
+
+//Reference types (Objectss)
+// let person = {
+//     name: 'Abdur Raffay',
+//     age: 24
+// };
+// console.log(person);
+
+//Dot Notation
+// person.name = 'A Raffay';
+// console.log(person.name);
+
+//Bracket Notation 
+// person['name'] = 'Naveed';
+// console.log(person.name); 
+
+//ARRAYS
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1   ;
+// console.log(selectedColors);
+// console.log(selectedColors.length ); //ARRAY
+
+
+//FUNCTIONS
+// function greet(name, lastName) {  //PARAMETER
+//     console.log('Hello ' + name + '' + lastName);
+// }
+
+// greet('Abdur', ' Raffay'); //ARGUMENT
+// greet('Naveed', ' Iqbal');  //FUNCTIONS
+
+
+//TYPES OF FUNCTIONS
+//PERFORMING A TASK
+// function greet(name, lastName) {  //PARAMETER
+//     console.log('Hello ' + name + '' + lastName);
+// }
+
+//CALCULATING A VALUE
+// function square(number) {
+//  return number * number;
+// }
+// // let number = square(2);
+// console.log(square(2)); //CALCULATING A VALUE
